45f98a470804b32b29adc8f132e15653c6bcc3b4
James Alfafara <jsalfafara@student.apc.edu.ph>
2023-02-10 15:23:30 +0800

    Final (fixed bug)

--- week3/main/Guests/guests.php
+++ week3/main/Guests/guests.php
@@ -18,7 +18,7 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     echo "id: " . $row["id"] . " - Name: " . $row["name"] . " - Email " . $row["email"]. "<br>";
-    
+  }
 } else {
   echo "0 results";
 }
